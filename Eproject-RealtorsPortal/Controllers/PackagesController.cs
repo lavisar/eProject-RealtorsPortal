34ed6042e82774d89876e0593638805f2fb1d2f4
PhamLong123 <90559727+PhamLong123@users.noreply.github.com>
2022-12-28 19:55:25 +0700

    update show hide function of PhoneContact and EmailContact

--- Eproject-RealtorsPortal/Controllers/PackagesController.cs
+++ Eproject-RealtorsPortal/Controllers/PackagesController.cs
@@ -8,6 +8,7 @@ namespace Eproject_RealtorsPortal.Controllers
     {
         LQHVContext LQHVContext = new LQHVContext();
         List<Package> indexBox;
+        Package package;
         public IActionResult Index()
         {
             //Hiển thị trạng thái khi trạng thái == true / đã kích hoạt
@@ -15,5 +16,11 @@ namespace Eproject_RealtorsPortal.Controllers
             return View("Index", indexBox);
         }
 
+        public IActionResult packageDetails(long ID)
+        {
+            //Link qua trang details dựa theo ID
+            package = LQHVContext.Packages.Where(s => s.PackagesId == ID).FirstOrDefault();
+            return View("packageDetails", package);
+        }
     }
 }
