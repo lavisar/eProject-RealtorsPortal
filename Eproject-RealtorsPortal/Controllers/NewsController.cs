b80f390f67c9e422c94c17f14ebbb0613ea50a51
QuanLe0702 <quanle0702@gmail.com>
2022-12-31 16:53:01 +0700

    About Us and News

--- Eproject-RealtorsPortal/Controllers/NewsController.cs
+++ Eproject-RealtorsPortal/Controllers/NewsController.cs
@@ -7,25 +7,117 @@ namespace Eproject_RealtorsPortal.Controllers
 {
     public class NewsController : Controller
     {
+        LQHVContext LQHVContext = new LQHVContext();
+        List<News> indexList;
+        News news;
+        News ForDeleteNews;
         public IActionResult Index()
         {
-            LQHVContext lQHVContext = new LQHVContext();
-            List<News> list = lQHVContext.News.ToList();
+            List<News> list = LQHVContext.News.ToList();
             return View(list);
         }
         public IActionResult Details(int id)
         {
-            LQHVContext lQHVContext = new LQHVContext();
-            News news = lQHVContext.News.Where(s => s.NewsId == id).FirstOrDefault();
+            News news = LQHVContext.News.Where(s => s.NewsId == id).FirstOrDefault();
             return View("Details", news);
         }
         [HttpPost]
 
         public IActionResult Details(News model)
         {
-            LQHVContext lQHVContext = new LQHVContext();
-            News news = lQHVContext.News.Where(s => s.NewsId == model.NewsId).FirstOrDefault();
+            News news = LQHVContext.News.Where(s => s.NewsId == model.NewsId).FirstOrDefault();
             return View(news);
+        }   
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult createNews()
+        {
+            return View(new News());
         }
+        [HttpPost]
+        public IActionResult createNews(News model)
+        {
+            LQHVContext.News.Add(model);
+            if (LQHVContext.SaveChanges() == 1)
+            {
+                return RedirectToAction("listNews", "News");
+            }
+            return View("createNews", model);
+        }
+        public IActionResult listNews()
+        {
+            indexList = LQHVContext.News.ToList();
+            return View("listNews", indexList);
+        }
+
+        
+        [HttpGet]
+        public IActionResult Delete(long ID)
+        {
+            ForDeleteNews = LQHVContext.News.Where(p => p.NewsId == ID).FirstOrDefault();
+            if (ForDeleteNews != null)
+            {
+                LQHVContext.News.Remove(ForDeleteNews);
+                if (LQHVContext.SaveChanges() > 0)
+                {
+                    TempData["msg"] = "Delete successfully";
+                    return RedirectToAction("listNews", "News");
+                }
+            }
+
+            List<News> indexList = LQHVContext.News.ToList();
+            TempData["msg"] = "Delete failed";
+            return View("listNews", indexList);
+        }
+
+        public void UploadImage(HttpContext context)
+        {
+            if (context.Request.Method == "POST")
+            {
+                var file = context.Request.Form.Files[0];
+                if (file != null)
+                {
+                    // Read the file and save it to a desired location
+                    using (var stream = new FileStream("path/to/save/file.jpg", FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+            }
+        }
+
+        public IActionResult updateNews(long ID)
+        {
+            using (var context = new LQHVContext())
+            {
+                var data = context.News.Where(x => x.NewsId == ID).SingleOrDefault();
+                return View(data);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult updateNews(long ID, News model)
+        {
+            using (var context = new LQHVContext())
+            {
+                var data = context.News.FirstOrDefault(x => x.NewsId == ID);
+                if (data != null)
+                {
+                    data.NewsTitle = model.NewsTitle;
+                    data.NewsDate = model.NewsDate;
+                    data.NewsDesc = model.NewsDesc;
+                    data.NewsContent = model.NewsContent;
+                    data.NewsImage = model.NewsImage;
+                    context.SaveChanges();
+                    return RedirectToAction("listNews", "News");
+                }
+                else
+                    return View("listNews", data);
+            }
+        }
+
     }
 }
